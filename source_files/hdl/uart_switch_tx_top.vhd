7b673e780a3c1dc100612ea76e2e8b47124b4db0
imran <imranrizwan1115@gmail.com>
2026-04-26 15:35:16 +0200

    Integrate 7-segment display to show active switch values
    - uart_switch_tx_top.vhd: Added a combinatorial decoder to translate the 3-bit switch input into active-low 7-segment LED patterns. Added the `digit_sel` port to multiplex the display and isolate the output to a single digit, resolving the floating pin ghosting issue.
    
    - basys3_switch_tx.xdc: Mapped the hardware constraints for the 8 cathode pins (LEDs) and 4 anode pins (digit_sel) to the physical Basys 3 pins.

--- source_files/hdl/uart_switch_tx_top.vhd
+++ source_files/hdl/uart_switch_tx_top.vhd
@@ -21,21 +21,60 @@ USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY uart_switch_tx_top IS
     PORT (
-        clk     : IN STD_LOGIC;
-        sw      : IN STD_LOGIC_VECTOR (2 DOWNTO 0);     -- for SW 2,1,0
-        btnC    : IN STD_LOGIC;                         -- centre push btn on the Basys3 board
-        RsTx    : OUT STD_LOGIC                         -- UART transmit pin connected to PC
+        clk         : IN STD_LOGIC;
+        sw          : IN STD_LOGIC_VECTOR (2 DOWNTO 0);     -- for SW 2,1,0
+        btnC        : IN STD_LOGIC;                         -- centre push btn on the Basys3 board
+        RsTx        : OUT STD_LOGIC;                        -- UART transmit pin connected to PC
+        LEDs        : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);     -- represents a 7-segment display on the board
+        digit_sel   : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)      -- 7-segment digit select
     );
 END uart_switch_tx_top;
 
 ARCHITECTURE structural OF uart_switch_tx_top IS
+    -- Bit patterns of the constants for the 7-segment LEDs (follows the hgfedcba pattern and is represented as high-active)
+    CONSTANT ZERO : STD_LOGIC_VECTOR(7 DOWNTO 0)    := "00111111";
+    CONSTANT ONE : STD_LOGIC_VECTOR(7 DOWNTO 0)     := "00000110"; 
+    CONSTANT TWO : STD_LOGIC_VECTOR(7 DOWNTO 0)     := "01011011";
+    CONSTANT THREE : STD_LOGIC_VECTOR(7 DOWNTO 0)   := "01001111";
+    CONSTANT FOUR : STD_LOGIC_VECTOR(7 DOWNTO 0)    := "01100110";
+    CONSTANT FIVE : STD_LOGIC_VECTOR(7 DOWNTO 0)    := "01101101";
+    CONSTANT SIX : STD_LOGIC_VECTOR(7 DOWNTO 0)     := "01111101";
+    CONSTANT SEVEN : STD_LOGIC_VECTOR(7 DOWNTO 0)   := "00000111";
+    CONSTANT EIGHT : STD_LOGIC_VECTOR(7 DOWNTO 0)   := "01111111";
+    CONSTANT NINE : STD_LOGIC_VECTOR(7 DOWNTO 0)    := "01101111";
+
     -- Internal signals
     SIGNAL send_s       : STD_LOGIC := '0';
     SIGNAL tx_data_s    : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
-
 BEGIN 
     -- concurrent ASCII conversion
     tx_data_s <= "00110" & sw;
+    -- turn ON only the third digit (from the left)
+    digit_sel <= "1101";
+
+    PROCESS(sw) 
+        VARIABLE led_v : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    BEGIN 
+        CASE sw IS
+            WHEN "001" =>
+                led_v := ONE;
+            WHEN "010" =>
+                led_v := TWO;
+            WHEN "011" =>
+                led_v := THREE;
+            WHEN "100" =>
+                led_v := FOUR;
+            WHEN "101" =>
+                led_v := FIVE;
+            WHEN "110" =>
+                led_v := SIX;
+            WHEN "111" =>
+                led_v := SEVEN;
+            WHEN OTHERS => 
+                led_v := ZERO;
+        END CASE;
+        LEDs <= NOT(led_v);
+    END PROCESS;
 
     -- Instantiate the edge detector to detect button press
     edge_detector_inst : ENTITY work.edge_detector PORT MAP(
--- source_files/xdc/basys3_switch_tx.xdc
+++ source_files/xdc/basys3_switch_tx.xdc
@@ -3,6 +3,10 @@ set_property PACKAGE_PIN W5 [get_ports clk]
     set_property IOSTANDARD LVCMOS33 [get_ports clk]
     create_clock -add -name clk -period 10.00 -waveform {0 5} [get_ports clk]
 
+# USB-UART Interface
+set_property PACKAGE_PIN A18 [get_ports RsTx]
+    set_property IOSTANDARD LVCMOS33 [get_ports RsTx]
+
 # Switches (SW2, SW1, SW0)
 set_property PACKAGE_PIN W16 [get_ports {sw[2]}]
     set_property IOSTANDARD LVCMOS33 [get_ports {sw[2]}]
@@ -15,6 +19,30 @@ set_property PACKAGE_PIN V17 [get_ports {sw[0]}]
 set_property PACKAGE_PIN U18 [get_ports btnC]
     set_property IOSTANDARD LVCMOS33 [get_ports btnC]
 
-# USB-UART Interface
-set_property PACKAGE_PIN A18 [get_ports RsTx]
-    set_property IOSTANDARD LVCMOS33 [get_ports RsTx]
+# 7-segment LED 
+set_property PACKAGE_PIN W7 [get_ports {LEDs[0]}]					
+	set_property IOSTANDARD LVCMOS33 [get_ports {LEDs[0]}]
+set_property PACKAGE_PIN W6 [get_ports {LEDs[1]}]					
+	set_property IOSTANDARD LVCMOS33 [get_ports {LEDs[1]}]
+set_property PACKAGE_PIN U8 [get_ports {LEDs[2]}]					
+	set_property IOSTANDARD LVCMOS33 [get_ports {LEDs[2]}]
+set_property PACKAGE_PIN V8 [get_ports {LEDs[3]}]					
+	set_property IOSTANDARD LVCMOS33 [get_ports {LEDs[3]}]
+set_property PACKAGE_PIN U5 [get_ports {LEDs[4]}]					
+	set_property IOSTANDARD LVCMOS33 [get_ports {LEDs[4]}]
+set_property PACKAGE_PIN V5 [get_ports {LEDs[5]}]					
+	set_property IOSTANDARD LVCMOS33 [get_ports {LEDs[5]}]
+set_property PACKAGE_PIN U7 [get_ports {LEDs[6]}]					
+	set_property IOSTANDARD LVCMOS33 [get_ports {LEDs[6]}]
+set_property PACKAGE_PIN V7 [get_ports {LEDs[7]}]							
+	set_property IOSTANDARD LVCMOS33 [get_ports {LEDs[7]}]
+
+# The four 7-segment Anodes
+set_property PACKAGE_PIN U2 [get_ports {digit_sel[0]}]                    
+    set_property IOSTANDARD LVCMOS33 [get_ports {digit_sel[0]}]
+set_property PACKAGE_PIN U4 [get_ports {digit_sel[1]}]                    
+    set_property IOSTANDARD LVCMOS33 [get_ports {digit_sel[1]}]
+set_property PACKAGE_PIN V4 [get_ports {digit_sel[2]}]                    
+    set_property IOSTANDARD LVCMOS33 [get_ports {digit_sel[2]}]
+set_property PACKAGE_PIN W4 [get_ports {digit_sel[3]}]                    
+    set_property IOSTANDARD LVCMOS33 [get_ports {digit_sel[3]}]
